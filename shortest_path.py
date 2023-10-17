08363249c5465d3726604ff9ef9ec59268e6103f
An Grocki <angrocki02@gmail.com>
2023-10-16 23:57:21 -0400

    Draft shortest path

--- shortest_path.py
+++ shortest_path.py
@@ -1,14 +1,9 @@
-graph_1 = [[0,2,6,0,0,0,0], # vertex 0 
-           [2,0,0,5,0,0,0], # vertex 1
-           [6,0,0,8,0,0,0], # vertex 2
-           [0,5,8,0,0,0,0], # vertex 3
-           [0,0,0,10,0,6,2], # vertex 4
-           [0,0,0,15,6,0,6], # vertex 5
-           [0,0,0,0,0,0,0]] # vertex 6
-           
-           
-
-Class Graph(): 
+"""
+This script does this...
+"""
+import math
+   
+class Graph():
     """
     Dikjstra's algorithm 
     
@@ -16,39 +11,81 @@ Class Graph():
         graph: A matrix representing a graph                         weighted edges 
         vertex: An integer representing the number of vertices in the graph 
     """
+
+    INF = math.inf
     def __init__(self, graph):
-    """
-    Initialize the attributes of the class
-    """
-    
+        """
+        Initialize the attributes of the class
+        """
         self.graph = graph
         self.vertex = len(graph)
-    def getWeights(self, vi, vj):
-    """
-    Given two vertices, return the weight between the two
+    def get_weights(self, vi, vj):
+        """
+        Given two vertices, return the weight between the two
     
-    Args: 
-        vi: 
-    """
+        Args: 
+            vi: 
+        """
         weight = self.graph[vi][vj]
         return weight
-    def dijkstra(self, source, end): 
+    def dijkstra(self, source, end):
+        """
+        Dijkstra algorithm  
+        """
         visited_nodes = []
         unvisited_nodes = []
-        for i in range(0, len(graph)):
+        for i in range(self.vertex):
             unvisited_nodes.append(i)
-        shortest_path = [inf] * self.vertex 
+        shortest_path = [self.INF] * self.vertex
         shortest_path[source] = 0
         current_node = source
-        while last not in visited_nodes: 
-            threshold = 0
-            all_edges = []
-            edge_index = None
-            for vertex in self.graph[current_node]:
-                if (vertex > 0):
-                    all_edges.append(vertex)
-            min_edge = min(all_edges)
-            visited_nodes.append(min_edge)
-            unvisited_nodes.remove(current_node.index(min_edge))
-            current_node = min_edge
-            for v
+        prev_node = current_node
+        while end not in visited_nodes:
+            print(f"current node : {current_node}")
+            min_weight = 0
+            next_node = current_node
+            print(f"prev_node: {prev_node}")
+            for vertex in range(self.vertex):
+                weight = self.graph[current_node][vertex]
+                print(weight)
+                if weight != 0 and vertex != prev_node and vertex in unvisited_nodes:
+                    if min_weight == 0:
+                        min_weight = weight 
+                        next_node = vertex
+                        prev_node = current_node
+                        # print(min_weight, next_node)
+                    elif min_weight != 0 and weight < min_weight:
+                        
+                        min_weight = weight
+                        next_node = vertex
+                        prev_node = current_node
+                        # print(min_weight, next_node)   
+            print(f"next node: {next_node}")
+            print(f"shortest edge: {min_weight}")
+            visited_nodes.append(current_node)
+            unvisited_nodes.remove(current_node)
+            print(f"visted nodes: {visited_nodes}")
+            print(f"unvisited nodes: {unvisited_nodes}")
+            print(shortest_path)
+            for node in unvisited_nodes:
+                edge_weight = self.get_weights(current_node, node)
+                if edge_weight != 0:
+                    if shortest_path[current_node] + edge_weight < shortest_path[node]:
+                        shortest_path[node] = shortest_path[current_node] + edge_weight
+            print(shortest_path)
+            current_node = next_node
+        return shortest_path[end]
+    
+graph_1 = [[0,2,6,0,0,0,0], # vertex 0 
+           [2,0,0,5,0,0,0], # vertex 1
+           [6,0,0,8,0,0,0], # vertex 2
+           [0,5,8,0,0,0,0], # vertex 3
+           [0,0,0,10,0,6,2], # vertex 4
+           [0,0,0,15,6,0,6], # vertex 5
+           [0,0,0,0,0,0,0]] # vertex 6
+    
+g = Graph(graph_1)
+p = g.dijkstra(0,6)
+print(p)
+
+    
